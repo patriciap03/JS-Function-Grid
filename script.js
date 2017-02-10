ffd56cc9658b1bc217723197a684e19d8189e6c4
Patricia Perez <patricia.perez0301@gmail.com>
2017-02-10 14:17:22 +0000

    created a smiley face, an S and a flag using setColor

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 //Write your function calls below this line
+//four coners
+setColor (0,0, "#99ddff");
+setColor (0,9, "#99ddff");
+setColor (9,0, "#99ddff");
+setColor (9,9, "#99ddff");
+//smiley face
+setColor (0,1, "#FF69B4");
+setColor (0,3, "#FF69B4");
+setColor (2,1, "#FF69B4");
+setColor (3,1, "#FF69B4");
+setColor (3,2, "#FF69B4");
+setColor (3,3, "#FF69B4");
+setColor (2,3, "#FF69B4");
+//An S
+setColor (0,7, "#00FA9A");
+setColor (0,6, "#00FA9A");
+setColor (1,6, "#00FA9A");
+setColor (2,6, "#00FA9A");
+setColor (2,7, "#00FA9A");
+setColor (3,7, "#00FA9A");
+setColor (4,7, "#00FA9A");
+setColor (4,6, "#00FA9A");
+//flag
+setColor (5,1, "#006699");
+setColor (6,1, "#006699");
+setColor (7,1, "#006699");
+setColor (8,1, "#006699");
+setColor (9,1, "#006699");
+setColor (9,2, "#006699");
+setColor (8,3, "#006699");
+setColor (7,4, "#006699");
+setColor (6,3, "#006699");
+setColor (5,2, "#006699");
+setColor (6,2, "#006699");
+setColor (7,3, "#006699");
+setColor (8,2, "#006699");
+setColor (7,2, "#ffffff");
+setColor (5,3, "red");
+setColor (5,4, "red");
+setColor (5,5, "red");
+setColor (5,6, "red");
+setColor (5,7, "red");
+setColor (5,8, "red");
+setColor (7,5, "red");
+setColor (7,6, "red");
+setColor (7,7, "red");
+setColor (7,8, "red");
+setColor (9,3, "red");
+setColor (9,4, "red");
+setColor (9,5, "red");
+setColor (9,6, "red");
+setColor (9,7, "red");
+setColor (9,8, "red");
